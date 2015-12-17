7de32dbbedc9dd810c0965515e9375072ce45e92
Joshua Audibert <jaudibert13@gmail.com>
2015-12-16 22:51:07 -0500

    Event adding now works for the most part

--- Final.js
+++ Final.js
@@ -1,94 +1,91 @@
 var http = require('http')
+  , express = require('express')
   , _ = require('underscore')
   , fs = require('fs')
   , url = require('url')
   , path = require('path')
+  , bodyParser = require('body-parser');
 
+var app = express();
+var port = process.env.PORT || 8080;
 
-// FAKEBOOK
-var posts= [
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ 
+  extended: false 
+}));
+
+var events = [
   {
     activity: '(Event Name)',
     date: 'December 18th, 2015',
     description: '(Insert Event Description)',
     proposer: '(Your Name Here)',
-    likes: '(0)',
+    likes: 0,
   },
   {
     activity: '(Event Name)',
     date: 'December 18th, 2015',
     description: '(Insert Event Description)',
     proposer: '(Your Name Here)',
-    likes: '(0)',
+    likes: 0,
   }]
 
-var server = http.createServer( function (req, res) {
-  var uri = url.parse(req.url)
-
-  switch( uri.pathname ) {
-    case '/':
-      sendFile(res, 'Final.html')
-      break
-    case '/index.html':
-      sendFile(res, 'Final.html')
-      break
-    case '/postsPokemon':
-      viewPosts(req, res);
-      break;
-    case '/postsNewPost':
-    	sendPosts(req, res);
-    default:
-      res.end('404 not found')
-  }
-})
-
-
-function viewPosts(req, res) {
+app.get('/events', function (req, res) {
   var str = "";
   var compiled = _.template(
     "<div class='posts'>" +
-    "<h2><%= activity %></h2>" +
-    "<p>Date:<%= date %></p>" +
-    "<p>Description: <%= description %></p>" +
-    "<p>Proposed By: <%= proposer %></p>" +
-    "<p>Likes: <%= likes %></p>" +
+      "<h2><%= activity %></h2>" +
+      "<p>Date: <%= date %></p>" +
+      "<p>Description: <%= description %></p>" +
+      "<p>Proposed By: <%= proposer %></p>" +
+      "<p>Likes: <%= likes %></p>" +
     "</div>");
-  posts.forEach( function(p, i) {
+  events.forEach( function(p, i) {
     str += compiled(p);
   });
 
-  res.end( str );
-}
+  console.log(events);
 
-function sendPosts(req, res) {
+  res.end( str );
+});
+
+app.get('/getNewEvent', function(req, res) {
   var str = "";
   str += 
   "<h1> New Post</h1>" +
-  "<p>Event Name</p>" +
-  "<input type='text' name='Event Name' value=''>" +
-  "<br>" +
-  "<p>Description</p>" +
-  "<input type='text' name='Description' value=''>" +
-  "<br><br>" +
-  "<input type='submit' value='Insert'";
+  "<form onsubmit='return false;'>" + 
+    "<label for='eventname'>Event Name: </label>" +
+    "<input id='eventname' type='text' name='eventname'>" +
+    "<br>" +
+    "<input id='date' type='date'></input>" +
+    "<br>" +
+    "<label for='description'>Description: </label>" +
+    "<input id='description' type='text' name='Description' value=''>" +
+    "<br><br>" +
+    "<input type='button' value='Add Event' onclick='addEvent();' />" +
+  "</form>";
 
   res.end( str );
-}
+});
 
-function sendFile(res, filename) {
-  res.writeHead(200, {'Content-type': 'text/html'})
+app.post('/addEvent', function(req, res) {
+  console.log("Adding event");
+  var newEvent = {
+    activity: req.body.eventname,
+    date: req.body.date,
+    description: req.body.description,
+    proposer: "Placeholder",
+    likes: 0
+  };
+  console.log(newEvent);
+  events.unshift(newEvent);
 
-  var stream = fs.createReadStream(filename)
+});
 
-  stream.on('data', function(data) {
-    res.write(data);
-  })
+app.use('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/Final.html'));
+});
 
-  stream.on('end', function(data) {
-    res.end();
-    return;
-  })
-}
-
-server.listen(8080)
-console.log("Server is listening on 8080")
+app.listen(port, function() {
+  console.log('App is listening on port ' + port);
+});
